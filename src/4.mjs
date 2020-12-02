46eab7fbc7545a3a5e702119c48fb545bcba7beb
Juuso Heikkinen <jmheik@gmail.com>
2020-12-02 18:02:30 +0200

    Puzzle 4.

---
+++ src/4.mjs
@@ -0,0 +1,24 @@
+import { loadData3 } from './common.mjs'
+
+function validate(password, letter, firstPos, secondPos) {
+  const firstMatch = password[firstPos - 1] === letter
+  const secondMatch = password[secondPos - 1] === letter
+  return firstMatch ? !secondMatch : secondMatch
+}
+
+function parseData(data) {
+  return data.map(line => {
+    const [policy, password] = line.split(': ')
+    const [range, letter] = policy.split(' ')
+    const [firstPos, secondPos] = line.split('-')
+    return [password, letter, parseInt(firstPos), parseInt(secondPos)]
+  })
+}
+
+const data = await loadData3()
+const parsedData = parseData(data)
+const validPasswordCount = parsedData.reduce((count, dataRow) => {
+  return validate(...dataRow) ? count + 1 : count
+}, 0)
+
+console.log("RESULT", validPasswordCount)
