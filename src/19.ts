b0869283f355ba74ce95f4790b856bb51db6074a
Juuso Heikkinen <jmheik@gmail.com>
2020-12-29 14:42:25 +0200

    Finish puzzle 19.

--- src/19.ts
+++ src/19.ts
@@ -1,55 +1,5 @@
 import { loadData19 } from "./utils.ts";
 
-const testInput = [
-  "42: 9 14 | 10 1",
-  "9: 14 27 | 1 26",
-  "10: 23 14 | 28 1",
-  '1: "a"',
-  "11: 42 31",
-  "5: 1 14 | 15 1",
-  "19: 14 1 | 14 14",
-  "12: 24 14 | 19 1",
-  "16: 15 1 | 14 14",
-  "31: 14 17 | 1 13",
-  "6: 14 14 | 1 14",
-  "2: 1 24 | 14 4",
-  "0: 8 11",
-  "13: 14 3 | 1 12",
-  "15: 1 | 14",
-  "17: 14 2 | 1 7",
-  "23: 25 1 | 22 14",
-  "28: 16 1",
-  "4: 1 1",
-  "20: 14 14 | 1 15",
-  "3: 5 14 | 16 1",
-  "27: 1 6 | 14 18",
-  '14: "b"',
-  "21: 14 1 | 1 14",
-  "25: 1 1 | 1 14",
-  "22: 14 14",
-  "8: 42",
-  "26: 14 22 | 1 20",
-  "18: 15 15",
-  "7: 14 5 | 1 21",
-  "24: 14 1",
-  "",
-  "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
-  "bbabbbbaabaabba",
-  "babbbbaabbbbbabbbbbbaabaaabaaa",
-  "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
-  "bbbbbbbaaaabbbbaaabbabaaa",
-  "bbbababbbbaaaaaaaabbababaaababaabab",
-  "ababaaaaaabaaab",
-  "ababaaaaabbbaba",
-  "baabbaaaabbaaaababbaababb",
-  "abbbbabbbbaaaababbbbbbaaaababb",
-  "aaaaabbaabaaaaababaa",
-  "aaaabbaaaabbaaa",
-  "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
-  "babaaabbbaaabaababbaabababaaab",
-  "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",
-];
-
 type Rule = "a" | "b" | number[][];
 type Rules = {
   [key: number]: Rule;
@@ -106,12 +56,36 @@ function match(
   return 0;
 }
 
-function patchRules(rules: Rules) {
-  return {
-    ...rules,
-    8: [[42], [42, 8]],
-    11: [[42, 31], [42, 11, 31]],
-  };
+function match2(
+  input: string,
+  rules: Rules,
+): number {
+  // Special casing:
+  // - Rule 42 must match N times, N > 0
+  // - Rule 31 must match M times, 0 < M < N
+  let index = 0;
+  let matches42 = 0;
+  let matches31 = 0;
+  while (true) {
+    const nextIndex = match(input, index, rules, 42);
+    if (nextIndex === 0) {
+      break;
+    }
+    index = nextIndex;
+    matches42++;
+  }
+  if (index === input.length) {
+    return 0;
+  }
+  while (matches31 < matches42) {
+    const nextIndex = match(input, index, rules, 31);
+    if (nextIndex === 0) {
+      break;
+    }
+    index = nextIndex;
+    matches31++;
+  }
+  return index;
 }
 
 function solvePuzzle1(data: string[]) {
@@ -122,13 +96,10 @@ function solvePuzzle1(data: string[]) {
 }
 
 function solvePuzzle2(data: string[]) {
-  // match handles patched rules wrong
   const rules = getRules(data);
-  const patchedRules = patchRules(rules);
   const inputs = getInputs(data);
-  return inputs.filter((line) =>
-    match(line, 0, patchedRules, 0) === line.length
-  ).length;
+  return inputs.filter((line) => match2(line, rules) === line.length)
+    .length;
 }
 
 const data = await loadData19();
